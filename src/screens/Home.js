7f3242f742cbc477851cb643da643e3cb2926836
marcordonez <marc.ordonez@estudiantat.upc.edu>
2023-12-06 16:55:48 +0100

    Change style of some screens

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,6 +4,7 @@ import { isWithinInterval, isToday } from 'date-fns';
 import NavBar from "../components/NavBar";
 import { useNavigate } from 'react-router-dom';
 import Map from "../components/Map";
+import {  Card, CardBody, CardFooter, Typography, Button,} from "@material-tailwind/react";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -21,7 +22,18 @@ const Home = () => {
             'Content-Type': 'application/json',
           },
         });
-
+        console.log(result)
+        result[1].nom = "Cuestionario Estudio RADONCOM"
+        result[1].tags =  [
+          { name: 'Cáncer', color: '#2F9E09', backgroundColor: '#D9FFC4' },	
+          { name: 'Pulmón', color: '#6558d3', backgroundColor: '#f1eeff' },
+        ]; 
+        result[0].tags =  [
+          { name: 'Otros', color: '#FF2D2D', backgroundColor: '#FFE4E1' },
+        ]; 
+        result[0].duracio = '⌛ Duración: 5 minutos';
+        result[1].duracio = '⌛ Duración: 30 minutos';
+        result[1].descripcio += " Añado un poco más de información para que parezca más realista. Una única línea de descripción no es demasiado típico. Aquí se debería añadir una pequeña introducción sobre la temática del estudio y la información que se espera obtener."
         setEnquestes(result);
       } catch (error) {
         console.error("falla home", error);
@@ -43,27 +55,53 @@ const Home = () => {
     navigate(`/${enquestaId}/InfoPage`);
   };
 
+  // const tags = [
+  //   { name: 'Cáncer', color: '#2F9E09', backgroundColor: '#D9FFC4' },	
+  //   { name: 'Pulmón', color: '#6558d3', backgroundColor: '#f1eeff' },
+  //   // Add more tags as needed
+  // ];
+  
+
   return (
     <>
       <NavBar />
-      <div className="enquestes">
-        <h2> Selecciona una encuesta </h2>
-        {enquestes.map((enquesta) => (
-          <div key={enquesta.id}>
-            {enquestaDisponible(enquesta) ? (
-              <div className="surveyBox" style={{ display: 'flex', flexDirection: 'column', background: enquesta.color }}>
-                <p className="titol">{enquesta.nom}</p>
-                <p className="questionText">{enquesta.descripcio}</p>
-                <div className="buttonContainer">
-                  <button className='buttonini' onClick={() => handleStart(enquesta.id)}>
-                    Start
-                  </button>
-                </div>
-              </div>
+      <div className="contenidor">
+        <p className="titolHome"> Selecciona una encuesta </p>
+        <div className="cards">
+          {enquestes.map((enquesta) => (
+            <div key={enquesta.id}>
+              {enquestaDisponible(enquesta) ? (
+                  <div className="information [ cardEnquesta ]">
+                    <div className='tags'>
+                      {enquesta.tags.map((tag, index) => (
+                        <span
+                          key={index}
+                          className="tag"
+                          style={{
+                            color: tag.color,
+                            backgroundColor: tag.backgroundColor,
+                          }}
+                        >
+                          {tag.name}
+                        </span>
+                      ))}
+                    </div>
+                    <h2 className="titleHome">{enquesta.nom}</h2>
+                    <p className="infoHome">{enquesta.descripcio}</p>
+                    <p className="duracio">{enquesta.duracio}</p> 
+                    <button className="button" onClick={() => handleStart(enquesta.id)}>
+                      <span>Empezar</span>
+                      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="none">
+                        <path d="M0 0h24v24H0V0z" fill="none" />
+                        <path d="M16.01 11H4v2h12.01v3L20 12l-3.99-4v3z" fill="currentColor" />
+                      </svg>
+                    </button>
+                  </div>
             ) : null}
           </div>
         ))}
-        <Map />
+        </div>
+       <Map />
       </div>
     </>
   );
--- src/screens/TermsConditions.js
+++ src/screens/TermsConditions.js
@@ -35,104 +35,56 @@ const Terms = () => {
   }
 
   return (
-    <div className='h-full' style={{ backgroundColor: '#b9fbc0', height: '100%', width: '100%' }}>  
+    <>
     <NavBar />
-    <div style={styles.termscontainer} >
-      <div style={styles.card}>
-        <div style={styles.titleContainer}>
-          <h2 style={styles.titleText}>Términos y condiciones</h2>
-        </div>
-        {termsConditions.map((item, index) => (
-          <div key={index} style={styles.infoContainer}>
-            {index === 0 && (
-              <p style={styles.info}> {item} </p>
-            )}
+    <div className="contenidor">
+        <div className="cards">
+          <div>
+            <div className="information [ cardEnquesta ]">
+              <h2 className="titleHome">Términos y condiciones</h2>
+              
+              
+              {termsConditions.map((item, index) => (
+                <div key={index} className="infoHome" style={{color:'#5E5E5E'}}>
+                  {index === 0 && (
+                    <p style={styles.info}> {item} </p>
+                  )}
 
-            {(index !== 0 && index !== 4) && (
-              <div style={styles.listContainer}>
-                <li style={styles.info}> {item} </li>
+                  {(index !== 0 && index !== 4) && (
+                    <div style={styles.listContainer}>
+                      <li style={styles.info}> {item} </li>
+                    </div>
+                  )}
+                </div>
+              ))}
+              
+              <div style={{display:"flex", alignItems:"center", justifyContent: "center", flexDirection: 'row', borderTop: "1px solid #e0e0e0",}}>
+                <div className="switch-checkbox">
+                  <label className="switch">
+                    <input type="checkbox" onChange={() => setIsEnabled(!isEnabled)} />
+                    <span className="slider round"> </span>
+                  </label>
+                </div>
+                <p className="infoHome" style={{color:'#5E5E5E'}}>{termsConditions[termsConditions.length - 1]}</p>
               </div>
-            )}
-          </div>
-        ))}
-        <div style={styles.endContainer}>
-          {termsConditions.map((item, index) => (
-            <div key={index}>
-              {index === termsConditions.length - 1 ? (
-                <p style={styles.info}>{item}</p>
-              ) : null}
+
+
+              <button className="button" disabled={!isEnabled} onClick={() => handleContinue()}>
+                <span>Empezar</span>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="none">
+                  <path d="M0 0h24v24H0V0z" fill="none" />
+                  <path d="M16.01 11H4v2h12.01v3L20 12l-3.99-4v3z" fill="currentColor" />
+                </svg>
+              </button>
             </div>
-          ))}
+          </div>
         </div>
-          <div style={{display:"flex", alignItems:"center", justifyContent: "center"}}>
-            <div className="switch-checkbox">
-              <label className="switch">
-                <input type="checkbox" onChange={() => setIsEnabled(!isEnabled)} />
-                <span className="slider round"> </span>
-              </label>
-            </div>
-            <button
-              className="button buttonText"
-              style={{
-                ...styles.button,
-                backgroundColor: isEnabled ? "#08693e" : "gray"
-              }}
-              disabled={!isEnabled}
-              onClick={handleContinue}
-            >
-              Continuar
-            </button>
-          </div>
       </div>
-    </div>
-    </div>
+    </>
   );
 };
 
 const styles = {
-  termscontainer: {
-    display: "flex",
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: "100%",
-    backgroundColor: "#b9fbc0",
-    overflowX: "hidden",
-  },
-
-  card: {
-    backgroundColor: "white",
-    display: "flex",
-    flexDirection: "column",
-    margin: "20px",
-    border: "1px solid white",
-    borderRadius: "20px",
-    boxShadow: "0px 2px 15px 0px rgba(0, 0, 0, 0.35)",
-    minWidth: "95%",
-  },
-
-  titleContainer: {
-    padding: "10px",
-  },
-
-  titleText: {
-    fontSize: "30px",
-    fontWeight: "bold",
-    color: "#333333",
-    marginTop: "10px",
-    marginBottom: "4px",
-    justifyContent: "flex-start"
-  },
-
-  infoContainer: {
-    backgroundColor: "#ffffff",
-    flexDirection: "row",
-    padding: "5px",
-    marginRight: "5px",
-    marginLeft: "5px"
-  },
-
   info: {
     fontSize: "15px",
     textAlign: "justify",
@@ -141,47 +93,6 @@ const styles = {
 
   listContainer: {
     width: "100%",
-  },
-
-  listItem: {
-    display: "flex",
-    alignItems: "flex-start",
-  },
-
-  endContainer: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    borderTop: "1px solid #e0e0e0",
-    margin: 0,
-    padding: "5px",
-  },
-
-  button: {
-    borderRadius: "10px",
-    padding: "10px",
-    margin: "10px",
-    width: "30%", 
-    cursor: "pointer",
-  },
-
-  buttonText: {
-    fontSize: "18px",
-    fontWeight: "bold",
-    color: "#ffffff",
-    textAlign: "center"
-  },
-  
-  '@media (max-width: 768px)': {
-    titleText: {
-      fontSize: "24px", // Reducir el tamaño del título para pantallas más pequeñas
-    },
-    listItem: {
-      width: "100%", // Hacer que los elementos de la lista ocupen el 100% del ancho
-    },
-    button: {
-      width: "100%", // Hacer que el botón ocupe el 100% del ancho
-    },
   },
 };
 
