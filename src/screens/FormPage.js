4269249c2f14cf4a4f631994698e30a59fe972f0
marcordonez <marc.ordonez@estudiantat.upc.edu>
2023-12-11 21:50:35 +0100

    fix current section

--- src/screens/FormPage.js
+++ src/screens/FormPage.js
@@ -80,7 +80,7 @@ const FormPage = () => {
   const [sectionValid, setSectionValid] = useState(true);
 
   const initializeData = async () => {
-    setCurrentSection(0);
+    setCurrentSection(0); 
     const result = await handleInfoEnquesta();
     const apartats = await handleApartatsEnquesta();
     const seccio = await handleInfoApartat(apartats, currentSection);
